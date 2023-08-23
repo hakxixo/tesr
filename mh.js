77ef87e4753a557fe800fdd9b7e6dcab9de19a5c
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-23 18:33:28 +0800

    Add files via upload

---
+++ mh.js
@@ -0,0 +1,16 @@
+/*******************************
+脚本名称:  mh
+脚本作者：halxixo
+更新时间：2023年8月23日 下午6:30
+
+*******************************
+[rewrite_local]
+^http[s]?:\/\/mangaapi.manhuaren.com\/v1\/manga\/getSections url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/mh.js
+[mitm] 
+hostname = mangaapi.manhuaren.com
+
+*******************************/
+
+var body=$response.body;
+body = body.replace(/isMustPay\":.*/g,'isMustPay":0');
+$done(body);
