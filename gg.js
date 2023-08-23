7516d1f5b400a692d26b350a2d5e61266b53574a
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-23 16:52:14 +0800

    Update gg.js

--- gg.js
+++ gg.js
@@ -4,7 +4,7 @@
 
 *******************************
 [rewrite_local]
-^http[s]?:\/\/wx.appds.cn\/api\/posts url script-response-body gg.js
+^http[s]?:\/\/wx.appds.cn\/api\/posts url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/gg.js
 [mitm] 
 hostname = wx.appds.cn
 
