567c775789140e335fe5a4e2773c99de91cf0514
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-23 19:13:33 +0800

    Update mh.js

--- mh.js
+++ mh.js
@@ -12,10 +12,4 @@ hostname = mangaapi.manhuaren.com
 *******************************/
 var body=$response.body;
 body = body.replace(/isMustPay\":\d/g,'isMustPay":0');
-body = body.replace(/giftCoinBalance\":\d/g,'giftCoinBalance":999999');
-body = body.replace(/mangaCoinBalance\":\d/g,'mangaCoinBalance":999999');
-body = body.replace(/mangaCoin\":\d+/g,'mangaCoin":0');
-body = body.replace(/isLackOfBalance\":\d/g,'isLackOfBalance":0');
-body = body.replace(/remainingMangaCoin\":\d+/g,'remainingMangaCoin":999999');
-body = body.replace(/remainingGiftCoin\":\d+/g,'remainingGiftCoin":999999');
 $done(body);
