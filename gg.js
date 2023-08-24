d97bd3cd5736b886c869b802b9073a72ef03f4c3
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-24 16:38:03 +0800

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,13 +1,14 @@
 /*******************************
-脚本名称:  屌丝博客微信小程序破解
+脚本名称:  屌丝博客、木小木微信小程序破解
 脚本作者：hakxixo
 
 *******************************
 [rewrite_local]
 ^http[s]?:\/\/wx.appds.cn\/api\/posts url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/gg.js
+^http[s]?:\/\/gd.hseon.cn\/api\/posts\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/gg.js
 [mitm] 
 hostname = wx.appds.cn
-
+hostname = gd.hseon.cn
 *******************************/
 
 var body=$response.body;
