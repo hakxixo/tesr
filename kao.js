b9bbca05f3477670b800ffc363191f615a7be639
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-24 21:42:30 +0800

    Update and rename ky.js to kao.js

--- kao.js
+++ kao.js
@@ -6,11 +6,12 @@
 #小程序://考研调剂/wVQHIryWBstf4lH
 *******************************
 [rewrite_local]
-^http[s]?:\/\/tiaoji.hanxiaocong.cn\/api\/member url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/ky.js
-^http[s]?:\/\/yysss23.piggyzzz.com\/mainApi\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/ky.js
-[mitm] 
-hostname = tiaoji.hanxiaocong.cn,yysss23.piggyzzz.com
+^http[s]?:\/\/tiaoji.hanxiaocong.cn\/api\/member url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/kao.js
+^http[s]?:\/\/yysss23.piggyzzz.com\/mainApi\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/kao.js
+^http[s]?:\/\/zzsss23new.piggyzzz.com\/mainApi\/firstpage\/ url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/kao.js
 
+[mitm] 
+hostname = tiaoji.hanxiaocong.cn,*.piggyzzz.com
 *******************************/
 var body=$response.body;
 body = body.replace(/is_vip\":\d+/g,'is_vip":1').replace(/haveauth":\d/g, 'haveauth":1').replace(/oncheck":\d/g, 'oncheck":0');
