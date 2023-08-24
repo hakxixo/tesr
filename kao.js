70e1704a4adb8695a80fc1550e5ceb80bfa78f13
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-24 21:51:13 +0800

    Update kao.js

--- kao.js
+++ kao.js
@@ -8,10 +8,10 @@
 [rewrite_local]
 ^http[s]?:\/\/tiaoji.hanxiaocong.cn\/api\/member url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/kao.js
 ^http[s]?:\/\/yysss23.piggyzzz.com\/mainApi\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/kao.js
-^http[s]?:\/\/zzsss23new.piggyzzz.com\/mainApi\/firstpage\/ url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/kao.js
+^http[s]?:\/\/zzsss23new.piggyzzz.com\/mainApi\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/kao.js
 
 [mitm] 
-hostname = tiaoji.hanxiaocong.cn,yysss23.piggyzzz.com, zzsss23new.piggyzzz.com
+hostname = tiaoji.hanxiaocong.cn,*.piggyzzz.com
 *******************************/
 var body=$response.body;
 body = body.replace(/is_vip\":\d+/g,'is_vip":1').replace(/haveauth":\d/g, 'haveauth":1').replace(/oncheck":\d/g, 'oncheck":0');
