e544e5b54b0f0e26312d52164c233edb7833fdc0
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-24 21:34:13 +0800

    Update ky.js

--- ky.js
+++ ky.js
@@ -1,8 +1,9 @@
 /*******************************
-脚本名称:  “考研调剂”微信小程序
+脚本名称:  考研小程序
 脚本作者：hakxixo
 更新时间：2023年8月21日 下午5:00
-
+#小程序://英语刷刷刷/WQVUXaATQxUjNIc
+#小程序://考研调剂/wVQHIryWBstf4lH
 *******************************
 [rewrite_local]
 ^http[s]?:\/\/tiaoji.hanxiaocong.cn\/api\/member url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/ky.js
