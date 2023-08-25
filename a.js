3da9ad3b28e1a68bb5c02a1fe8a9a8a94d9d1e7b
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-25 11:38:39 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,17 +1,15 @@
 /*******************************
-脚本名称:  
+脚本名称: 屌丝博客小程序破解
 脚本作者：halxixo
-更新时间：2023年8月25日 上午11:28
 
 *******************************
 [rewrite_local]
-^http[s]?:\/\/wx.appds.cn\/api\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/a.js 
+^http[s]?:\/\/wx.appds.cn\/api\/* url script-response-body a.js
 [mitm] 
 hostname = *.appds.cn
 
 *******************************/
 
-
 var body=$response.body;
-body = body.replace(/adRead\":\w+/g,'adRead":false');
+body = body.replace(/adRead\":\w+/g,'adRead":false').replace(/status\":\w+/g,'status":true');
 $done(body);
