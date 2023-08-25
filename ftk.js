af32005e7b83c3f51d70f2253a39537b89bd9726
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-25 18:13:04 +0800

    Add files via upload

---
+++ ftk.js
@@ -0,0 +1,13 @@
+/*******************************
+脚本名称:  法题库破解
+脚本作者：hakxixo
+*******************************
+[rewrite_local]
+^http[s]?:\/\/*.kaoyanhui.com.cn\/index.php\/api\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/ftk.js
+[mitm] 
+hostname = *.kaoyanhui.com.cn
+
+*******************************/
+var body=$response.body;
+body = body.replace(/permission\":\d/g,'permission":1').replace(/is_vip\":\d/g,'is_vip":1').replace(/status\":\d/g,'status":1');
+$done(body);
