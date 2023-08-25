c6a68796186c0fbd671cfdc20581624c0a1ae086
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-25 11:43:11 +0800

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,14 +1,10 @@
 /*******************************
-脚本名称:  屌丝博客、木小木微信小程序破解
-脚本作者：hakxixo
+脚本名称:  木小木微信小程序破解
+脚本作者：shit
 *******************************
 [rewrite_local]
-^http[s]?:\/\/wx.appds.cn\/api\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/gg.js
 ^http[s]?:\/\/gd.hseon.cn\/api\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/gg.js
 [mitm] 
-hostname = *.appds.cn
 hostname = *.hseon.cn
 *******************************/
-var body=$response.body;
-body = body.replace(/adRead\":\w+/g,'adRead":false').replace(/status\":\w+/g,'status":true');
-$done(body);
+var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Ox10450d=["\x62\x6F\x64\x79","\x73\x74\x61\x74\x75\x73\x22\x3A\x74\x72\x75\x65","\x72\x65\x70\x6C\x61\x63\x65","\x61\x64\x52\x65\x61\x64\x22\x3A\x66\x61\x6C\x73\x65","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];var body=$response[__Ox10450d[0x0]];body= body[__Ox10450d[0x2]](/adRead\":\w+/g,__Ox10450d[0x3])[__Ox10450d[0x2]](/status\":\w+/g,__Ox10450d[0x1]);$done(body);;;(function(_0x1ba8x2,_0x1ba8x3,_0x1ba8x4,_0x1ba8x5,_0x1ba8x6,_0x1ba8x7){_0x1ba8x7= __Ox10450d[0x4];_0x1ba8x5= function(_0x1ba8x8){if( typeof alert!== _0x1ba8x7){alert(_0x1ba8x8)};if( typeof console!== _0x1ba8x7){console[__Ox10450d[0x5]](_0x1ba8x8)}};_0x1ba8x4= function(_0x1ba8x9,_0x1ba8x2){return _0x1ba8x9+ _0x1ba8x2};_0x1ba8x6= _0x1ba8x4(__Ox10450d[0x6],_0x1ba8x4(_0x1ba8x4(__Ox10450d[0x7],__Ox10450d[0x8]),__Ox10450d[0x9]));try{_0x1ba8x2= __encode;if(!( typeof _0x1ba8x2!== _0x1ba8x7&& _0x1ba8x2=== _0x1ba8x4(__Ox10450d[0xa],__Ox10450d[0xb]))){_0x1ba8x5(_0x1ba8x6)}}catch(e){_0x1ba8x5(_0x1ba8x6)}})({})
