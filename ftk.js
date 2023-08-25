8cdf8aa4bd066d6a66a6b26362b95480198370e7
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-25 19:38:07 +0800

    Update ftk.js

--- ftk.js
+++ ftk.js
@@ -1,14 +1,13 @@
 /*******************************
 脚本名称:  法题库
 脚本作者：hakxixo
-
 *******************************
 [rewrite_local]
-^http[s]?:\/\/*.kaoyanhui.com.cn\/index.php\/api\/chapter\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/ftk.js
+^http[s]?:\/\/*.kaoyanhui.com.cn\/index.php\/api\/chapter\/* url script-response-body ftk.js
 [mitm] 
 hostname = *.kaoyanhui.com.cn
-
 *******************************/
+
 var body=$response.body;
 body = body.replace(/permission\":\d/g,'permission":1');
 $done(body);
