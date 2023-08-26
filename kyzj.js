af5c7f5b2ac946a6ba7e6beab7e31789c5526c08
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-26 17:14:42 +0800

    Create kyzj.js

---
+++ kyzj.js
@@ -0,0 +1,17 @@
+/*******************************
+脚本名称:  考研备考之家小程序
+脚本作者：hakxixo
+*******************************
+[rewrite_local]
+
+^http[s]?:\/\/*.next.bspapp.com\/tiaoji\/isVip url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/kyzj.js
+^http[s]?:\/\/*.next.bspapp.com\/politic\/judgeReciteVip url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/kyzj.js
+
+[mitm] 
+
+hostname = *.next.bspapp.com
+
+*******************************/
+
+var body = $response.body.replace(/.+/g,'true')
+$done({ body });
