c27bfbba6d9a2ee99c22f0cf022a00c151ed93d7
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-26 11:31:29 +0800

    Update ftk.js

--- ftk.js
+++ ftk.js
@@ -1,12 +1,19 @@
 /*******************************
 脚本名称:  法题库
 脚本作者：hakxixo
+更新时间：2023年8月26日 上午11:23
 *******************************
 [rewrite_local]
-^http[s]?:\/\/*.kaoyanhui.com.cn\/index.php\/api\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/ftk.js
+
+^http[s]?:\/\/newapi.kaoyanhui.com.cn\/index.php\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/ftk.js
+
 [mitm] 
+
 hostname = newapi.kaoyanhui.com.cn
+
 *******************************/
-var body=$response.body;
-body = body.replace(/permission"\s*:\s*0/g,'permission":1');
-$done(body);
+
+var body = $response.body.replace(/permission":\d/g,'permission":1')
+.replace(/is_vip":\s*\d/g,'is_vip":1')
+.replace(/is_vip":\d/g,'is_vip":1')
+$done({ body });
