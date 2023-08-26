173bf7fd090152fb1dc8af34aee1d4cdda431fff
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-26 09:21:48 +0800

    Add files via upload

---
+++ kao1.js
@@ -0,0 +1,14 @@
+/*******************************
+脚本名称: 考研备考之家小程序破解
+脚本作者：hakxixo
+#小程序://考研备考之家/DBXW6hAcUBMUVuG
+*******************************
+[rewrite_local]
+^http[s]?:\/\/*.next.bspapp.com\/politic\/judgeReciteVip url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/kao1.js
+[mitm] 
+hostname = *.next.bspapp.com
+
+*******************************/
+var body=$response.body;
+body = body.replace(/.+/g,'true');
+$done(body);
