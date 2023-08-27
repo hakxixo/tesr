7016bc40c8df7dcbbb149a9fbe86d7b2c62eea29
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-27 22:19:12 +0800

    Update zz.js

--- zz.js
+++ zz.js
@@ -4,7 +4,8 @@
 #小程序://苍盾政治/FvSgXhusHvOhveg
 *******************************
 [rewrite_local]
-^http[s]?:\/\/zztk.cdky100.vip\/(apitk|apiyz)\/(home|user\/quanxian) url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/zz.js
+^http[s]?:\/\/zztk.cdky100.vip\/apitk\/home url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/zz.js
+^http[s]?:\/\/zztk.cdky100.vip\/apiyz\/user\/quanxian url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/zz.js
 [mitm] 
 hostname = zztk.cdky100.vip
 *******************************/
