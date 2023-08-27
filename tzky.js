b9a5d2a5e9569e769757409d160186203c81af65
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-08-27 23:14:05 +0800

    Create tzky.js

---
+++ tzky.js
@@ -0,0 +1,14 @@
+/*******************************
+脚本名称:  团子考研题库小程序破解
+脚本作者：hakxixo
+#小程序://考研题库/bKv01vfuohFHhXo
+*******************************
+[rewrite_local]
+^http[s]?:\/\/dev.miniapp.okaoyan.com\/api\/v1\/users\/info url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/take.js
+[mitm] 
+hostname = dev.miniapp.okaoyan.com
+
+*******************************/
+var body=$response.body;
+body = body.replace(/is_vip\":\w+/g,'is_vip":true');
+$done(body);
