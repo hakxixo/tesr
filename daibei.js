0183ba07b16ee914661b6f358440f0d07ee8715d
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-09-15 13:24:30 +0800

    Update daibei.js

--- daibei.js
+++ daibei.js
@@ -1,10 +1,10 @@
 /*******************************
-脚本名称:法硕带背微信小程序
+脚本名称:  法硕带背
 脚本作者：hakxixo
 #小程序://法硕带背/74xotxsK8pIk8bF
 *******************************
 [rewrite_local]
-^http[s]?:\/\/daibei.zhishikaoyan.com\/daibei\/res\/v1\/index\/info url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/daibei.js
+^http[s]?:\/\/daibei.zhishikaoyan.com\/daibei\/res\/v1\/(index\/info|chapter\/getList) url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/daibei.js
 [mitm] 
 hostname = daibei.zhishikaoyan.com
 *******************************/
