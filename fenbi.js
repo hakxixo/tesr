8ba94133815a4bc8d60f8c5b90820b5c6819953f
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-09-22 12:47:18 +0800

    Update fenbi.js

--- fenbi.js
+++ fenbi.js
@@ -1,12 +1,1 @@
-/*******************************
-脚本名称:  粉笔专业课小程序
-脚本作者：hakxixo
-#小程序://专业课/0CxzZ0t32nvAC8j
-*******************************
-[rewrite_local]
-^http[s]?:\/\/english-mini.fenbi.com\/* url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/fenbi.js
-[mitm] 
-hostname = english-mini.fenbi.com
-*******************************/
-var body = $response.body.replace(/"is_unlock":\s*\w+/g,"\"is_unlock\":1");
-$done(body);
+
