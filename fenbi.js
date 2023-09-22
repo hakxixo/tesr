fa80f21614d6a6fca83325605f9c498a56ff6b8f
hakxixo <116273166+hakxixo@users.noreply.github.com>
2023-09-22 12:31:11 +0800

    Create fenbi.js

---
+++ fenbi.js
@@ -0,0 +1,13 @@
+/*******************************
+脚本名称:  粉笔专业课小程序
+脚本作者：hakxixo
+#小程序://专业课/0CxzZ0t32nvAC8j
+*******************************
+[rewrite_local]
+^http[s]?:\/\/english-mini.fenbi.com\/xcx\/user\/questionBank url script-response-body https://raw.githubusercontent.com/hakxixo/tesr/main/fenbi.js
+[mitm] 
+hostname = english-mini.fenbi.com
+*******************************/
+var body=$response.body;
+body = body.replace(/is_unlock\":\s*([^,'is_unlock":"1"');
+$done(body);
